2d65cfe6225c4da243c345878e71043e5ccaf66e
noshluk2 <noshluk2@gmail.com>
2022-02-01 12:40:59 +0500

    Esp32 Serial Scripts added

---
+++ src/ESP32_scripts/b_ultrasonic_controller.ino
@@ -0,0 +1,48 @@
+/* 
+ * rosserial Publisher Example
+ * Sends Ultrasonic Sensor Values on ROS Master
+
+Written by : M.Luqman 
+Date : 1/Feb/2022
+ */
+
+#include <ros.h>
+#include <std_msgs/Int16.h>
+
+const int trig_Pin = 14;
+const int echo_Pin = 12;
+
+#define SPEED_OF_SOUND 0.034
+
+long duration;
+float dist_in_cm;
+
+ros::NodeHandle  nh;
+
+std_msgs::Int16 ultrasonic_values_msg;
+ros::Publisher ultrasonic_values_node("ultrasonic_Values_Topic", &ultrasonic_values_msg);
+
+void setup()
+{ 
+  pinMode(trig_Pin, OUTPUT); 
+  pinMode(echo_Pin, INPUT);
+  nh.initNode();
+  nh.advertise(ultrasonic_values_node);
+}
+
+void loop()
+{ 
+  digitalWrite(trig_Pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_Pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_Pin, LOW);
+  duration = pulseIn(echo_Pin, HIGH);
+  dist_in_cm = duration * SPEED_OF_SOUND/2;
+  ultrasonic_values_msg.data=dist_in_cm;
+  ultrasonic_values_node.publish(&ultrasonic_values_msg);
+  nh.spinOnce();
+  delay(100);
+}
+
+
